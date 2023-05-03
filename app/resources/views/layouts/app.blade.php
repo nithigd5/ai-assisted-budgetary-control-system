2016068a09a8a519fde92bd1664e1b3891ae4f12
nithi <nithi@mallow-tech.com>
2023-05-03 13:42:12 +0530

    Updated UI

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -13,10 +13,15 @@
     <link href="assets/img/favicon.png" rel="icon">
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
+    <script href="{{ asset('js/jquery.easing-1.4.1.min.js') }}" rel="stylesheet"></script>
+    <script href="{{ asset('js/jquery.min.js') }}" rel="stylesheet"></script>
+
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
           rel="stylesheet">
+
+    <link href="{{ asset('css/fontawsome-free-all.min.css') }}" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
