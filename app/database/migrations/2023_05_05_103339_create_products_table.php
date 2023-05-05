fb556a3cd3580e71e3fa138190267da85e0ed306
nithi <nithi@mallow-tech.com>
2023-05-05 16:21:38 +0530

    Added Migrations, models, factories, controllers for expenses,products. Updated Dashboard by removing unwanted items

---
+++ app/database/migrations/2023_05_05_103339_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->string('category');
+            $table->string('type');
+            $table->float('min_price');
+            $table->float('max_price');
+            $table->string('brand');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
