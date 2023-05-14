eb7d6dd438273dd034650fd0817fb493078f472b
Nithi GD <nithi@mallow-tech.com>
2023-05-14 12:32:34 +0530

    added sentiments

--- app/database/factories/ExpenseFactory.php
+++ app/database/factories/ExpenseFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Http;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Expense>
@@ -18,12 +19,28 @@ class ExpenseFactory extends Factory
      */
     public function definition(): array
     {
+        $sentiment = '';
+
+        $feedback = $this->faker->randomElement(['happy', 'sad', 'worst', 'ok', 'shit', 'not bad', 'worthless', 'wonderful', 'nice', 'good', 'its better', 'unsatisfied', 'satisfied']);
+
+        try {
+            $response = Http::timeout(3)->post(
+                config('app.api_host') . '/purchases/analyze-sentiment' ,
+                ['text' => $feedback]
+            );
+            $sentiment = $response->json()['TextBlob_Analysis'];
+
+        } catch (\Exception $exception) {
+
+        }
+
         return [
             'user_id' => User::factory(),
             'product_id' => Product::factory(),
             'price' => $this->faker->numberBetween(50, 100),
             'mode' => $this->faker->randomElement(['online', 'offline']),
-            'feedback' => $this->faker->randomElement(['happy', 'sad', 'worst', 'ok', 'shit', 'not bad', 'worthless', 'wonderful', 'nice', 'good', 'its better', 'unsatisfied', 'satisfied'])
+            'feedback' => $feedback,
+            'sentiment' => $sentiment
         ];
     }
 }
